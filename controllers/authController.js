0f01b5ac16fd00fc06374ea26f54ebdd977b4da9
Serveone-Tech <lokendrasingh271193@gmail.com>
2026-02-05 17:50:05 +0530

    change token security in login

--- controllers/authController.js
+++ controllers/authController.js
@@ -62,9 +62,8 @@ export const login = async (req, res) => {
     console.log("Generated token:", token);
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
-      path: "/",
+      secure: true,
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
