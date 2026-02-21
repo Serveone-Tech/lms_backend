555bc59df3d86e959e89c403f2e012f519579676
Serveone-Tech <lokendrasingh271193@gmail.com>
2026-02-21 13:27:38 +0530

    deploy test

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.use("/api/certificate", certificateRouter);
 
 /* ================= HEALTH CHECK ================= */
 app.get("/", (req, res) => {
-  res.send("Hello From Arvind");
+  res.send("Hello From Zalgo");
 });
 
 /* ================= START SERVER ================= */
